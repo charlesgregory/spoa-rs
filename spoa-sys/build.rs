e63fcc45d3844b972825633101f3b8d804ca16fa
charlesgregory <tgregory@forgebiologics.com>
2024-11-12 15:21:36 -0500

    added new cargo feature to enable cpu dispatch

--- spoa-sys/build.rs
+++ spoa-sys/build.rs
@@ -9,16 +9,27 @@ fn main() {
         "OFF"
     };
 
-    let dst = cmake::Config::new("spoa")
+    let use_dispatch = if std::env::var("CARGO_FEATURE_DISPATCH").is_ok() {
+        "ON"
+    } else {
+        "OFF"
+    };
+    let mut dst = cmake::Config::new("spoa")
         .define("spoa_build_exe", "OFF")
         .define("spoa_build_tests", "OFF")
+        .define("spoa_install", "OFF")
         .define("spoa_optimize_for_native", "OFF")
         .define("spoa_use_simde", use_simde)
+        .define("spoa_generate_dispatch", use_dispatch)
         .build();
+    dst = dst.join("build");
     let lib64 = dst.join("lib64");
     let lib = dst.join("lib");
     println!("cargo:rustc-link-search=native={}", lib64.display());
     println!("cargo:rustc-link-search=native={}", lib.display());
+    if use_dispatch == "ON" {
+        println!("cargo:rustc-link-lib=static=cpu_features");
+    }
     println!("cargo:rustc-link-lib=static=spoa");
 
     println!("cargo:rerun-if-changed=spoa/CMakeLists.txt");
